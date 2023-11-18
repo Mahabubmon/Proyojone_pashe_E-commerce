d46a1d91d1a6bf9cbcb31b72ead7c87e485209ce
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-18 22:17:46 +0600

    button

--- login.php
+++ login.php
@@ -12,24 +12,27 @@
 <body>
     <div class="conatainer d-flex justify-content-center align-items-center vh-100">
         <div class="row">
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                        placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                        else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Login</button>
-            </form>
+            <div class="card" style="width: 25rem; height: 15rem;">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="inputEmail1" aria-describedby="emailHelp"
+                            placeholder="Enter email">
+
+                    </div>
+                    <div class="form-group">
+                        <label for="Password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your password with anyone
+                            else.</small>
+                    </div>
+                    <!-- <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div> -->
+                    <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Login</button>
+                </form>
+            </div>
         </div>
     </div>
 
