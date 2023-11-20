618ac742f5eff31f931463024cda1da4cc4a7578
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-20 12:50:08 +0600

    sanitize

--- classes/Basic.php
+++ classes/Basic.php
@@ -1,7 +1,34 @@
 <?php
 abstract class Basic
 {
+    public function sanitize($var, $type)
+    {
+        $filter = false;
+        switch ($type) {
+            case 'email':
+                $var = substr($var, 0, 254);
+                $filter = FILTER_VALIDATE_EMAIL;
+                break;
+            case 'int':
+                $filter = FILTER_VALIDATE_INT;
+                break;
+            case 'boolean':
+                $filter = FILTER_VALIDATE_BOOLEAN;
+                break;
+            case 'ip':
+                $filter = FILTER_VALIDATE_IP;
+                break;
+            case 'url':
+                $filter = FILTER_VALIDATE_URL;
+                break;
+            case 'string':
+            default:
+                $filter = FILTER_SANITIZE_STRING;
+                break;
 
+        }
+        return $filter = trim(filter_var($var, $filter));
+    }
 }
 
 
--- classes/User.php
+++ classes/User.php
@@ -5,10 +5,11 @@ class User extends Basic
     private $dbConnection;
     function __construct($db)
     {
+        $this->dbConnection = $db;
     }
     public function validation($fname, $lname, $userName, $email, $password, $passwordConfirm)
     {
-
+        $fname=$this
     }
 }
 ?>
