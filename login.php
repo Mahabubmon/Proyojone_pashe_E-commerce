fe9727ff95270c4808660c52c2e929c81ead7e81
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-23 19:29:25 +0600

    field-config

--- login.php
+++ login.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'config.php';
+s
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,29 +9,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="container">
         <div class="text">
-            <input class="input1" type="text" placeholder="Username">
-            <input class="input2" s type="password" placeholder="Password">
+            <input class="input1" id="userName" type="text" placeholder="Username" required>
+            <input class="input2" id="pass" type="password" placeholder="Password" required>
             <p>Forgot Password</p>
-            <input type="submit" name="" id="submitLogin" value="Login">
+            <input type="submit" name="" id="submitLogin" value="Login" onclick="loginForm()">
             <p>Not A Mamber? <span>register</span></p>
         </div>
     </div>
 
     <script src="main.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
+
+<script>
+
+    function loginForm() {
+
+    }
+
+</script>
