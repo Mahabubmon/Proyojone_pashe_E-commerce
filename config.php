9ff9f846d6721ae56b54d25501f8847016a0b71c
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-24 14:56:17 +0600

    config.php

--- config.php
+++ config.php
@@ -1,18 +1,16 @@
 <?php
-define("DBNAME", "admin");
-define("DBUSER", "root");
-define("DBPASS", "");
-define("DBHOST", "localhost");
+$sname = "localhost";
+$uname = "root";
+$password = "";
 
-try {
-    $db = 'mysql:host=' . DBHOST . ';dbname=' . DBNAME . ';utf8';
-    $db = new PDO($db, DBUSER, DBPASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$dbname = "admin";
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-    // echo "Your page is connected with database successfully...";
-} catch (PDOException $e) {
-    echo "Issue-> Connection failed" . $e->getMessage();
+$conn = new mysqli($sname, $uname, $password, $dbname);
+
+if (!$conn) {
+    echo "Connection failed";
 }
+
 
 
 ?>
--- main.js
+++
@@ -1,3 +0,0 @@
-function loginForm() {
-  console.log("okk");
-}
