de32df8e4ec63736acb66182cce11acbb5016b50
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-24 14:24:37 +0600

    login_page_congig

--- index.php
+++ index.php
@@ -1,35 +1,45 @@
-<?php require 'vendor/autoload.php';
-
-if (isset($_SESSION["userName"])) {
-    header("Lcation.php");
-}
+<?php
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin-Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <form action="index.php" method="post">
+        <h2>LOGIN</h2>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error">
+                <?php echo $_GET['error']; ?>
+            </p>
+        <?php } ?>
+        <label for="">User Name</label>
+        <input type="text" name="userName" placeholder="User Name">
+        <label for="">Password</label>
+        <input type="password" name="password" placeholder="Password">
 
-    <h1>This is the index Page</h1>
-    <script src="main.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-        crossorigin="anonymous"></script>
+        <button type="submit">Login</button>
+    </form>
+
+
+
+
+
+
 </body>
 
 </html>
+
+<script>
+
+    function loginForm() {
+
+    }
+
+</script>
--- login.php
+++ login.php
@@ -1,45 +1,24 @@
 <?php
+session_start();
+include "config.php";
+
+if (isset($_POST["userName"]) && $_POST["password"]) {
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+$userName = validate($_POST["userName"]);
+$pass = validate($_POST["pasword"]);
+
+
+if (empty($userName)) {
+    hearder("Location: index.php?erro=User Name is required");
+}
+
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <h2>LOGIN</h2>
-        <?php if (isset($_GET['error'])) { ?>
-            <p class="error">
-                <?php echo $_GET['error']; ?>
-            </p>
-        <?php } ?>
-        <label for="">User Name</label>
-        <input type="text" name="userName" placeholder="User Name">
-        <label for="">Password</label>
-        <input type="password" name="password" placeholder="Password">
-
-        <button type="submit">Login</button>
-    </form>
-
-
-
-
-
-
-</body>
-
-</html>
-
-<script>
-
-    function loginForm() {
-
-    }
-
-</script>
