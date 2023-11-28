2ad60a14e2a3fc75179ec07a0b1de9dfa2429e84
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-28 21:17:38 +0600

    code_cleaing

--- index.php
+++ index.php
@@ -5,20 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div class="container">
-        <div class="card">
-            <form action="">
-                <input type="text" placeholder="Enter the Email" name="email"><br><br>
-                <input type="password" placeholder="Enter the Password" name="pass"><br><br>
 
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </div>
 </body>
 
 </html>
