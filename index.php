ba40269fac67c528fc744cc10ced5b3fdf17c610
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-28 23:50:01 +0600

    Table__ui_setup

--- index.php
+++ index.php
@@ -54,9 +54,29 @@
                         <td>Rahman</td>
                         <td>mahabub@test.com</td>
                         <td>
-                            <a href="profile.php?id=1">View</a>
+                            <a class="btn btn-primary" href="profile.php?id=1">View</a>
                         </td>
                     </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>Rahul</td>
+                        <td>Al</td>
+                        <td>mrahul@test.com</td>
+                        <td>
+                            <a class="btn btn-primary" href="profile.php?id=1">View</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>arif</td>
+                        <td>Rahman</td>
+                        <td>arif@test.com</td>
+                        <td>
+                            <a class="btn btn-primary" href="profile.php?id=1">View</a>
+                        </td>
+                    </tr>
+
+
                 </table>
             </div>
         </div>
