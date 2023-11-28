2a4f3b8bf549841377034ce879ac1646ba8649fe
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-28 23:24:04 +0600

    index_page_setup

--- index.php
+++ index.php
@@ -5,9 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="inc/bootstrap.min.css">
+    <script src="inc/jquery.min.js"></script>
+    <script src="inc/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php"></a>
+                </div>
+                <ul class="nav navbar-nav pull-right">
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="">Logout</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="register.php">Register</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        Body Text
+        <div class="well">
+            <h3>
+                <span class="pull-right">Like Us</span>
+            </h3>
+        </div>
+    </div>
 
 </body>
 
