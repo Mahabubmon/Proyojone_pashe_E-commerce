325cd94731ffa2e699fe1663e9de0250f7396f95
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-29 22:43:41 +0600

    db_connection

--- lib/Database.php
+++ lib/Database.php
@@ -1,6 +1,12 @@
 <?php
 class Database
 {
+    private $hostdb = "localhost";
+    private $userdb = "root";
+    private $password = "";
+    private $namedb = "db_ir";
+    public $pdo;
+
 
     public function __construct()
     {
