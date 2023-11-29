dedd7805457999bf30be2b213f6a873965e32d6d
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-29 22:14:49 +0600

    register_page

--- register.php
+++ register.php
@@ -1,11 +1,19 @@
 <?php include 'inc/header.php' ?>
 <div class="panel panel-default">
     <div class="panel-headeing">
-        <h2>User Login</h2>
+        <h2>User Registration</h2>
     </div>
     <div class="panel-body">
         <form action="" method="post">
             <div style="max-width:600px; margin:0 auto;">
+                <div class="form-group">
+                    <label for="name">Your Name</label>
+                    <input type="text" id="name" name="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="usernaem">User Name</label>
+                    <input type="text" id="username" name="username" class="form-control">
+                </div>
                 <div class="form-group">
                     <label for="email">Email Address</label>
                     <input type="text" id="email" name="email" class="form-control">
@@ -14,7 +22,7 @@
                     <label for="password">Password</label>
                     <input type="text" id="password" name="password" class="form-control">
                 </div>
-                <button type="submit" name="login" class="btn btn-success">Login</button>
+                <button type="submit" name="register" class="btn btn-success">Submitt</button>
         </form>
     </div>
 </div>
