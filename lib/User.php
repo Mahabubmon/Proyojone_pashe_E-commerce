1b0990d350cb9577470518f5f32bcc5b770a0abc
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-30 21:03:37 +0600

    validation

--- lib/User.php
+++ lib/User.php
@@ -14,21 +14,31 @@ class User
         $name = $data['name'];
         $username = $data['username'];
         $email = $data['email'];
-        
         $password = md5($data['password']);
+        $chk_email = $this->emailCheck($email);
 
-        if($name == ''|| $username == '' || $email == ''|| $password == ''){
+        if ($name == '' || $username == '' || $email == '' || $password == '') {
             $msg = "<div class = 'alert alert-danger'><strong>Error !</strong> Field must note be empty</div>";
             return $msg;
         }
-        if(strlen($username) < 3){
+        if (strlen($username) < 3) {
             $msg = "<div class = 'alert alert-danger'><strong>Error !</strong> User Name is too short</div>";
             return $msg;
-    }elseif('[^a-z0-9_-]/i'){
-         $msg = "<div class = 'alert alert-danger'><strong>Error !</strong> User Name must only content 
+        } elseif ('[^a-z0-9_-]/i') {
+            $msg = "<div class = 'alert alert-danger'><strong>Error !</strong> User Name must only content 
          alphanumerical,dashes and underscoress</div>";
-    }
+        }
 
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $msg = "<div class = 'alert alert-danger'><strong>Error !</strong> The Email Address is Not Valid</div>";
+            return $msg;
+        }
+    }
+    public function emailheck($email)
+    {
+        $sql = "SELECT email FROM tbl_user WHERE email = :email";
+        $query = $this->db->pdo->prepare($sql);
+    }
 }
 
 
