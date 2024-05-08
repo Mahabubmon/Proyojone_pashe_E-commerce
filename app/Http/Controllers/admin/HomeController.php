fbd4029baef7ef0b45d4822593ccac9dbbec7e68
Mahabubmon <mahabubmon@gmail.com>
2024-05-08 15:15:16 +0600

    fresh new project installation with admin login authentication

---
+++ app/Http/Controllers/admin/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        $admin = Auth::guard('admin')->user();
+
+        echo "Hello!" . $admin->name . '<a href="' . route('admin.logout') . '">Logout</a>';
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+}
