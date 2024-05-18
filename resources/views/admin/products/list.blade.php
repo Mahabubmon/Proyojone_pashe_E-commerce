c0fbd7a08eb5bc35858d9120bbecee4afdaa7f1d
Mahabubmon <mahabubmon@gmail.com>
2024-05-18 08:58:29 +0600

    product module done

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -120,7 +120,8 @@
                                                                                 </path>
                                                                             </svg>
                                                                         </a>
-                                                                        <a href="#" class="text-danger w-4 h-4 mr-1">
+                                                                        <a href="#" onclick="deleteProduct({{ $product->id }})"
+                                                                            class="text-danger w-4 h-4 mr-1">
                                                                             <svg wire:loading.remove.delay="" wire:target=""
                                                                                 class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg"
                                                                                 viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -155,10 +156,9 @@
 
 
 <script>
-    function deleteBrand(id) {
+    function deleteProduct(id) {
 
-
-        var url = '{{route('brands.delete', 'ID')}}';
+        var url = '{{route('products.delete', 'ID')}}';
         var newUrl = url.replace("ID", id);
 
 
@@ -168,12 +168,12 @@
                 type: 'DELETE',
                 data: {},
                 dataType: 'json',
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
                 success: function (response) {
                     if (response['status']) {
-                        window.location.href = "{{route('brands.index')}}";
+                        window.location.href = "{{route('products.index')}}";
+                    } else {
+                        window.location.href = "{{route('products.index')}}";
+
                     }
                 }
 
