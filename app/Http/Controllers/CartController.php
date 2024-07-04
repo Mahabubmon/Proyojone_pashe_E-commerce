f4665bc6e468afc3ec4ff8a03ea2e555f6e37d96
Mahabubmon <mahabubmon@gmail.com>
2024-07-04 23:35:43 +0600

    comment

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -398,7 +398,7 @@ class CartController extends Controller
                 $orderItem->total = $item->total;
                 $orderItem->save();
             }
-
+            //session for getting the message on action
             session()->flash('success', 'You Have successfully placed your order.');
 
 
