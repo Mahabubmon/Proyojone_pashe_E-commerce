3e655f65c76a24e4f262d324ed380b559fcf516d
Mahabubmon <mahabubmon@gmail.com>
2024-07-20 22:32:55 +0600

    list page of order

--- resources/views/admin/orders/detail.blade.php
+++ resources/views/admin/orders/detail.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 <!-- section to show the details of order -->
+<section class="section-5 pt-3 pb-3 mb-3 bg-white">
+    <div class="container">
+        <div class="light-font">
+            <ol class="breadcrumb primary-color mb-0">
+                <li class="breadcrumb-item"><a class="white-text" href="#">My Account</a></li>
+                <li class="breadcrumb-item">Settings</li>
+            </ol>
+        </div>
+    </div>
+</section>
 
 @endsection
 
