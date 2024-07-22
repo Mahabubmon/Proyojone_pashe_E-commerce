2066c38005b8bcf07680ef7f1d7b7d6c5aff8daa
Mahabubmon <mahabubmon@gmail.com>
2024-07-22 21:36:16 +0600

    remove some code for cleaning

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -93,15 +93,11 @@
                 <button class="navbar-toggler menu-btn" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
-                    <!-- <span class="navbar-toggler-icon icon-menu"></span> -->
                     <i class="navbar-toggler-icon fas fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <!-- <li class="nav-item">
-          				<a class="nav-link active" aria-current="page" href="index.php" title="Products">Home</a>
-        			</li> -->
-
+                       
                         @if(getCategories()->isNotEmpty())
 
                             @foreach (getCategories() as $category)
