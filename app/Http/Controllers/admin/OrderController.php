888db53b9894065723ce8b594d8f8f1c07cb9d50
Mahabubmon <mahabubmon@gmail.com>
2024-07-29 22:12:24 +0600

    controller data fetcj

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
+        $orders = Order::latest();
+        $orders = $orders->leftJoin('users', 'users.id', 'orders.user_id');
+
+        if ($request->get('keyword') != "") {
+            $orders = $orders->where('users.name', 'like', '%' . $request->keyword . '');
+            $orders = $orders->orWhere('users.email', 'like', '%' . $request->keyword . '%');
+            $orders = $orders->orWhere('orders.id', 'like', '%' . $request->keyword . '%');
+        }
+
         return view('admin.orders.list');
     }
 
