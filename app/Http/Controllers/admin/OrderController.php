4e72c0915cabb60e742cd87266a719dd79847d16
Mahabubmon <mahabubmon@gmail.com>
2024-08-08 21:56:07 +0600

    delail

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -33,8 +34,13 @@ class OrderController extends Controller
             ->where('orders.id', $orderId)
             ->leftJoin('countries', 'countries.id', 'orders.country_id')
             ->first();
+
+
+        $orderItem = OrderItem::where('order_id', $orderId)->get();
+
         return view('admin.orders.detail', [
-            'order' => $order
+            'order' => $order,
+            'orderItem' => $orderItem
         ]);
     }
 }
