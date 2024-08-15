bc782a07c4d5fd7f95c7b8b837ac857a2f9e1133
Mahabubmon <mahabubmon@gmail.com>
2024-08-15 23:36:19 +0600

    order controller change order status

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -45,8 +45,20 @@ class OrderController extends Controller
     }
 
 
-    public function changeOrderStatus(Request $request)
+    public function changeOrderStatus(Request $request, $orderId)
     {
+        $order = Order::find($orderId);
+        $order->status = $request->status;
+        $order->shipped_date = $request->shipped_date;
+        $order->save();
 
+        $message = 'Order status updated successfully';
+
+        session()->flash('status', $message);
+
+        return response()->json([
+            'status' => true,
+            'message' => $message
+        ]);
     }
 }
