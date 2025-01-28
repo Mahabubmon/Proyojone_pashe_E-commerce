d85f7e22e0aa228aa3971a7ede85809a4e516182
mahabubmon <mahabubmon@gmail.com>
2025-01-28 14:59:06 +0600

    resolve

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,17 +2,37 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
-
+        DB::table('categories')->insert([
+            [
+                'name' => 'Category 1',
+                'slug' => 'category-1',
+                'image' => 'category1.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'Category 2',
+                'slug' => 'category-2',
+                'image' => 'category2.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'Category 3',
+                'slug' => 'category-3',
+                'image' => 'category3.jpg',
+                'status' => 1,
+            ],
+        ]);
     }
 }
